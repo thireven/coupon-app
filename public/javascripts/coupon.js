43fbd418a41a1d4779321f93fef4958b7ae62298
Jessica Erazo <jessicamerazo@gmail.com>
2018-07-07 11:33:47 -0700

    pushing refactoring of code

--- public/javascripts/coupon.js
+++ public/javascripts/coupon.js
@@ -12,16 +12,15 @@ function renderCoupons(res){
   return`
     <section role="region" class="coupon-container js-coupon-container" data-id="${res._id}">
         <div class="coupon-actions-nav">
-        <button type="button" class="js-edit-coupon-btn" data-toggle="modal" data-target="#editCouponModal">
+        <button type="button" class="btn-transparent js-edit-coupon-btn" data-toggle="modal" data-target="#editCouponModal">
           <span class="icons icon-budicon-classic edit-icon js-edit-icon" alt="edit-icon"></span>
         </button>
             <span class="icons icon-budicon-classic-2 delete-icon js-delete-icon" alt="delete-icon"></span>
         </div>
         <h2 class="coupon-merchant-name">${res.merchantName}</h2>
         <p class="coupon-code">${res.code}</p>
-        <p class="coupon-expiration-date">Valid til ${res.expirationDate}</p>
+        <p class="coupon-expiration-date">${res.expirationDate}</p>
         <p class="coupon-description">${res.description}</p>
-        <p class="coupon-id">${res._id}</p>
     </section>`;
 }
 
@@ -30,7 +29,6 @@ function watchSubmitAddNewCouponHandler(){
         e.preventDefault();
         console.log('you added a coupon');
         $('#addNewCouponModal').modal('toggle');
-        //do a call to post coupon to db with endpoint /coupon
         sendAddCouponDataToAPI();
     });
 }
@@ -48,7 +46,7 @@ function sendAddCouponDataToAPI() {
         expirationDate: $('#expirationDate').val(),
         description: $('#description').val()
       },
-      dataType:'json',///////////////////JUST ADDED THIS/////////////////////////
+      dataType:'json',
   		success: function(res){
         $('#merchantName').val('');
         $('#code').val('');
@@ -72,9 +70,9 @@ function sendAddCouponDataToAPI() {
 function watchDeleteBtnHandler() {
     $('#js-list-coupons-section').on('click','.js-delete-icon', function(e) {
         e.preventDefault();
-        const couponId = $(this).closest('.js-coupon-container').data('id');
+        const couponId = $(this).parent().parent().attr('data-id');
         console.log(`The coupon id: ${couponId}`);
-        $(this).closest('.js-coupon-container').remove();
+        $(this).parent().parent().remove();
         sendCouponToDeleteFromApi(couponId);
     });
 }
@@ -92,7 +90,7 @@ function sendCouponToDeleteFromApi(id) {
         console.log(`you successfully deleted a coupon`);
       },
       error: function(err) {
-        console.log(`Something happened when trying to delete ${res}`);
+        console.log(`Something happened when trying to delete ${err}`);
       }
     });
 }
@@ -100,14 +98,36 @@ function sendCouponToDeleteFromApi(id) {
 function getCouponid(){
   $('#js-list-coupons-section').on('click','.js-edit-icon', function(e) {
       e.preventDefault();
-      const couponId = $(this).closest('.js-coupon-container').data('id');
+      const couponId = $(this).parent().parent().parent().attr('data-id');
       console.log(`The coupon id of the edit coupon ${couponId}`);
+
+      //get the values currently in the input fields for that getCouponid
+      const couponObject = $(this).parent().parent().parent();
+      console.log(couponObject);
+
+      const merchantNameText = $(couponObject).find('h2.coupon-merchant-name').text();
+      console.log(merchantNameText);
+
+      const codeText = $(couponObject).find('p.coupon-code').text();
+      console.log(codeText);
+
+      const expirationDateText = $(couponObject).find('p.coupon-expiration-date').text();
+      console.log(expirationDateText);
+
+      const descriptionText = $(couponObject).find('p.coupon-description').text();
+      console.log(descriptionText);
+
+      $('.input-merchantName').val(merchantNameText);
+      $('.input-code').val(codeText);
+      $('.input-expirationDate').val(expirationDateText);
+      $('.input-description').val(descriptionText);
+
+      //pull the values that the user types in the inputs
       watchEditCouponHandler(couponId)
   });
 }
 
 function watchEditCouponHandler(id){
-  console.log(id);
   $('#js-submit-edit-coupon-btn').on('click', function(e) {
       e.preventDefault();
       console.log('you want to update a coupon');
@@ -117,6 +137,7 @@ function watchEditCouponHandler(id){
 }
 
 function sendCouponToEditFromApi(id){
+  let _couponId = id;
   console.log(`If I got here then I should edit this id: ${id} on the DB`);
     $.ajax({
       url: `/coupon/${id}`,
@@ -125,49 +146,24 @@ function sendCouponToEditFromApi(id){
         xhr.setRequestHeader('Authorization', `Bearer ${localStorage.getItem('Token')}`);
       },
       data: {
-        merchantName: $('.merchantName').val(),
-        code: $('.code').val(),
-        expirationDate: $('.expirationDate').val(),
-        description: $('.description').val()
+        merchantName: $('.input-merchantName').val(),
+        code: $('.input-code').val(),
+        expirationDate: $('.input-expirationDate').val(),
+        description: $('.input-description').val()
       },
       dataType: 'json',
       success: function(res) {
-        console.log(`you successfully updated a coupon`);
+        console.log(`you successfully updated a coupon: ${_couponId}`);
 
-        // const merchantNameInput = $(this).find('.merchantName');
-        // const codeInput = $(this).find('.code');
-        // const expirationDateInput = $(this).find('.expirationDate');
-        // const descriptionInput = $(this).find('.description');
-        //
-        // const merchantNameVal = merchantNameInput.val();
-        // console.log(merchantNameVal);
+        var merchantName = $('.input-merchantName').val();
+        var inputCode = $('.input-code').val();
+        var expirationDate = $('.input-expirationDate').val();
+        var inputDescription = $('.input-description').val();
 
-        let form = event.target;
-        let currentId = form.getAttribute('id');
-
-        const taskInput = $(this).find('.merchantName' + currentId);
-
-        const taskValue = taskInput.val();
-
-
-        const editCoupon = {
-          taskTitle: taskValue,
-
-        };
-
-
-        //clears the form
-        $('.merchantName').val('');
-        $('.code').val('');
-        $('.expirationDate').val('');
-        $('.description').val('');
-
-
-
-        // $('.coupon-merchant-name').replaceWith(`<h2 class="coupon-merchant-name">chnaginhtext</h2>`);
-        // $('.coupon-code').replaceWith(`<p class="coupon-code">hiJessica</p>`);
-        // $('.coupon-expiration-date').replaceWith(`<p class="coupon-expiration-date">hiJessica</p>`);
-        // $('.coupon-description').replaceWith(`<p class="coupon-description">hiJessica</p>`);
+        $(`[data-id = ${_couponId}] .coupon-merchant-name`).html(merchantName);
+        $(`[data-id = ${_couponId}] .coupon-code`).html(inputCode);
+        $(`[data-id = ${_couponId}] .coupon-expiration-date`).html(expirationDate);
+        $(`[data-id = ${_couponId}] .coupon-description`).html(inputDescription);
 
 
         $('#js-msg-output').html(`<div class="alert alert-success alert-dismissible fade show text-center" role="alert">You have successfully edited a coupon!
@@ -186,6 +182,5 @@ function initalizeCouponApp() {
     watchSubmitAddNewCouponHandler();
     watchDeleteBtnHandler();
     getCouponid();
-    //watchEditCouponHandler();
 }
 $(initalizeCouponApp);
